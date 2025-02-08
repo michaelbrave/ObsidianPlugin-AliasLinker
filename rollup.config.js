4929a977c8e74d7817828f782b60633a86e7aef5
michaelbrave <michaelbrave@outlook.com>
2025-02-08 03:24:51 -0800

    fixes I  hope

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'main.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
+  plugins: [typescript()],
+  external: ['obsidian'],
+};
